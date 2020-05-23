40ea2c761b2be3b634d51af9f4788c97ecbd23c1
Eugene Annienkov <eugene.annienkov@onereach.com>
2020-05-23 22:48:57 +0300

    chore: add simple analyze information

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import { bufferCount, pluck } from 'rxjs/operators';
 import { getTradeStream } from './trades';
+import fs from 'fs';
 
 const SYMBOLS = {
     BTCUSDT: 'btcusdt',
@@ -20,5 +21,21 @@ getTradeStream({
 })
     .pipe(pluck('price'), bufferCount(10, 1))
     .subscribe(trade => {
+        if (trade[trade.length - 1] - trade[0] >= 2) {
+            fs.appendFile(
+                'message.txt',
+                `Buy ${trade[trade.length - 1]};\n`,
+                err => {
+                    if (err) throw err;
+                    console.log('The buy price were appended to file!');
+                }
+            );
+        }
+        if (trade[0] - trade[trade.length - 1] >= 2) {
+            fs.appendFile('message.txt', `Sell ${trade[0]};\n`, err => {
+                if (err) throw err;
+                console.log('The sell price were appended to file!');
+            });
+        }
         console.log(trade);
     });
