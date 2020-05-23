ca8224c913056e4bd36001f7b2589ccebe276ad7
dmitry.honcharuk <dmitry.honcharuk@gmail.com>
2020-05-23 17:52:42 +0300

    basic ws connection

---
+++ index.js
@@ -0,0 +1,19 @@
+const getTradeStream = require('./trades');
+
+const SYMBOLS = {
+  BTCUSDT: 'btcusdt',
+  BNBUSDT: 'bnbusdt',
+  BNBBTC: 'bnbbtc',
+};
+
+const RESOURCES = {
+  AGG_TRADE: 'aggTrade',
+  TICKER: 'ticker',
+};
+
+getTradeStream({
+  symbol: SYMBOLS.BNBUSDT,
+  resource: RESOURCES.AGG_TRADE,
+}).subscribe((trade) => {
+  console.log(trade);
+});
