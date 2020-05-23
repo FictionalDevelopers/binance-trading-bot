8a994f0c958544589bd1c7adb5cdc803e0e99977
cheslasvegas <cheslasvegas@gmail.com>
2020-05-24 00:07:01 +0300

    Added buy price, sell price and profit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
-import { bufferCount, pluck } from 'rxjs/operators';
-import { getTradeStream } from './trades';
+import {bufferCount, pluck} from 'rxjs/operators';
+import {getTradeStream} from './trades';
 import fs from 'fs';
+
+
+let canISell = false;
+let buysCounter = 0;
+let buyPrice = null;
 
 const SYMBOLS = {
     BTCUSDT: 'btcusdt',
@@ -21,7 +26,10 @@ getTradeStream({
 })
     .pipe(pluck('price'), bufferCount(10, 1))
     .subscribe(trade => {
-        if (trade[trade.length - 1] - trade[0] >= 2) {
+        if (trade[trade.length - 1] - trade[0] >= 2 && !canISell) {
+            buysCounter++;
+            buyPrice = trade[trade.length - 1];
+            canISell = true;
             fs.appendFile(
                 'message.txt',
                 `Buy ${trade[trade.length - 1]};\n`,
@@ -31,11 +39,15 @@ getTradeStream({
                 }
             );
         }
-        if (trade[0] - trade[trade.length - 1] >= 2) {
-            fs.appendFile('message.txt', `Sell ${trade[0]};\n`, err => {
-                if (err) throw err;
-                console.log('The sell price were appended to file!');
-            });
+        if (trade[0] - trade[trade.length - 1] >= 2 && canISell) {
+            if (buysCounter !== 0) {
+                canISell = false;
+                const profit = trade[0] / buyPrice * 100 > 100 ? trade[0] / buyPrice * 100 - 100 : -1 * (100 - trade[0] / buyPrice * 100);
+                fs.appendFile('message.txt', `Sell ${trade[0]};\nProfit ${profit}%\n`, err => {
+                    if (err) throw err;
+                    console.log('The sell price were appended to file!');
+                });
+            }
         }
         console.log(trade);
     });
