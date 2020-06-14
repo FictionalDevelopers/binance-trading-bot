475d79dd2a9b14d58a5aa77d2cf4417cf5810e19
cheslasvegas <cheslasvegas@gmail.com>
2020-06-14 21:25:13 +0300

    Added price stream

--- src/strategy1.ts
+++ src/strategy1.ts
@@ -3,9 +3,10 @@ import { map, pluck } from 'rxjs/operators';
 import { format } from 'date-fns';
 
 import { connect } from './db/connection';
-import { SYMBOLS } from './constants';
+import { SYMBOLS, RESOURCES } from './constants';
 import { DATE_FORMAT } from './constants/date';
 import { getCandleStreamForInterval } from './api/candles';
+import { getTradeStream } from './api/trades.js';
 import { transformRsiToSignal } from './tools/rsi-tool';
 import { makeVerticalVolumeToolStream } from './tools/vertical-volume-tool';
 import { makeStrategy } from './strategies/make-strategy';
@@ -22,32 +23,40 @@ import { getDmiStream } from './indicators/dmi';
   const interval = '1m';
   const symbol = SYMBOLS.BTCUSDT;
 
-  const candlePrices$ = getCandleStreamForInterval(
-    SYMBOLS.BTCUSDT,
-    interval,
-  ).pipe(pluck('closePrice'), map(Number));
+  // const candlePrices$ = getCandleStreamForInterval(
+  //   SYMBOLS.BTCUSDT,
+  //   interval,
+  // ).pipe(pluck('closePrice'), map(Number));
 
-  const volumes$ = makeVerticalVolumeToolStream(
-    {
-      interval,
-      symbol,
-    },
-    {
-      minimalLatestCandleVolume: 30,
-      minimalPercentageIncrease: 20,
-    },
-  );
+  getTradeStream({
+    symbol: symbol,
+    resource: RESOURCES.TRADE,
+  })
+    .pipe(pluck('price'), map(Number))
+    .subscribe(data => console.log(data));
+
+  // const volumes$ = makeVerticalVolumeToolStream(
+  //   {
+  //     interval,
+  //     symbol,
+  //   },
+  //   {
+  //     minimalLatestCandleVolume: 30,
+  //     minimalPercentageIncrease: 20,
+  //   },
+  // );
 
   const rsiSignals$ = getRsiStream({
     symbol: SYMBOLS.BTCUSDT,
     period: 14,
     interval: '1m',
-  }).pipe(
-    transformRsiToSignal({
-      overbought: [50, 70],
-      oversold: [30, 50],
-    }),
-  );
+  }).subscribe(data => console.log('data', data));
+  //     .pipe(
+  //   transformRsiToSignal({
+  //     overbought: [50, 70],
+  //     oversold: [30, 50],
+  //   }),
+  // );
 
   getDmiStream({
     symbol: SYMBOLS.BTCUSDT,
@@ -55,12 +64,12 @@ import { getDmiStream } from './indicators/dmi';
     period: 14,
   }).subscribe(data => console.log('data', data));
 
-  const strategy$ = makeStrategy({
-    buyTools: [volumes$],
-    sellTools: [rsiSignals$],
-  });
+  // const strategy$ = makeStrategy({
+  //   buyTools: [volumes$],
+  //   sellTools: [rsiSignals$],
+  // });
 
-  let hasBought = false;
+  // let hasBought = false;
 
   await sendToRecipients(`INIT
   Bot started working at: ${format(new Date(), DATE_FORMAT)}
@@ -68,32 +77,32 @@ import { getDmiStream } from './indicators/dmi';
   symbol: ${symbol}
   `);
 
-  combineLatest(strategy$, candlePrices$).subscribe(
-    async ([strategySignalDetails, price]) => {
-      const date = format(new Date(), DATE_FORMAT);
-
-      if (!hasBought && strategySignalDetails.action === BUY) {
-        await sendToRecipients(`BUY
-          price: ${price}
-          date: ${date}
-          signals: ${JSON.stringify(strategySignalDetails.signals)}
-        `);
-
-        hasBought = true;
-      }
-
-      if (hasBought && strategySignalDetails.action === SELL) {
-        await sendToRecipients(`SELL
-          price: ${price}
-          date: ${date}
-          current profit:
-          total profit:
-        `);
-
-        hasBought = false;
-      }
-    },
-  );
+  // combineLatest(strategy$, candlePrices$).subscribe(
+  //   async ([strategySignalDetails, price]) => {
+  //     const date = format(new Date(), DATE_FORMAT);
+  //
+  //     if (!hasBought && strategySignalDetails.action === BUY) {
+  //       await sendToRecipients(`BUY
+  //         price: ${price}
+  //         date: ${date}
+  //         signals: ${JSON.stringify(strategySignalDetails.signals)}
+  //       `);
+  //
+  //       hasBought = true;
+  //     }
+  //
+  //     if (hasBought && strategySignalDetails.action === SELL) {
+  //       await sendToRecipients(`SELL
+  //         price: ${price}
+  //         date: ${date}
+  //         current profit:
+  //         total profit:
+  //       `);
+  //
+  //       hasBought = false;
+  //     }
+  //   },
+  // );
 })();
 
 process.on('unhandledRejection', async (reason: Error) => {
