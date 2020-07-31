6bc008fef1cca0de69972470919369ef096782e0
cheslasvegas <cheslasvegas@gmail.com>
2020-07-31 15:17:01 +0300

    Started changing the strat

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -59,7 +59,8 @@ import { getEmaStream } from './indicators/ema';
     fast1hEMA: 0,
     isDownTrend: false,
     rebuy: true,
-    sellInOverboughtArea: false,
+    isDirectionalMovementChanged: false,
+    directionalMovementSignalWeight: 0,
   };
 
   const trader = async pricesStream => {
@@ -123,24 +124,6 @@ import { getEmaStream } from './indicators/ema';
     //       );
     if (
       botState.status === 'buy' &&
-      rsi1hValue !== null &&
-      rsi1hValue > 50 &&
-      rsi1hValue <= 68 &&
-      rsi1mValue !== null &&
-      rsi1mValue > 50 &&
-      ema1mSignal === 1 &&
-      !indicatorsData.sellInOverboughtArea
-      // !indicatorsData.isDownTrend &&
-      // ema1mSignal === 1 &&
-      // ema1hSignal === 1 &&
-      // rsi1mValue <= 35 &&
-      // rsi1mValue !== null &&
-      // rsi1hValue < 68 &&
-      // rsi1hValue !== null &&
-      // rebuy
-      // mdi1mSignal === 1 &&
-      // adx1mSignal === 1 &&
-      // mdi1hSignal === 1
     ) {
       try {
         botState.updateState('status', 'isPending');
@@ -258,86 +241,6 @@ import { getEmaStream } from './indicators/ema';
         botState.updateState('status', 'sell');
       }
     }
-    if (
-      botState.status === 'sell' &&
-      ((rsi1mValue > 70 &&
-        indicatorsData.sellInOverboughtArea &&
-        expectedProfitPercent >= 0.5) ||
-        expectedProfitPercent >= 0.5)
-      // rsi1mValue >= 60 &&
-      // ema1mSignal === -1
-      // middle1mEMA > fast1mEMA &&
-      // adx1mSignal === -1 &&
-      // fast1mEMA < middle1mEMA &&
-      // (expectedProfitPercent >= 0.7 && fast1mEMA < middle1mEMA) ||
-      // ||
-      // ema1mSignal === -1)
-    ) {
-      try {
-        botState.updateState('status', 'isPending');
-        botState.updateState('buyPrice', null);
-        const amount = binance.roundStep(
-          Number(botState.availableCryptoCoin),
-          stepSize,
-        );
-        const order = await marketSell(symbol.toUpperCase(), +amount);
-        botState.updateState('order', order);
-        const { available: refreshedUSDTBalance } = await getBalances('USDT');
-        const currentProfit =
-          Number(refreshedUSDTBalance) - Number(botState.availableUSDT);
-        botState.updateState('currentProfit', currentProfit);
-        botState.updateState('availableUSDT', +refreshedUSDTBalance);
-        botState.updateState(
-          'totalProfit',
-          Number(refreshedUSDTBalance) - Number(initialUSDTBalance),
-        );
-        const { available: refreshedCryptoCoinBalance } = await getBalances(
-          cryptoCoin,
-        );
-        botState.updateState(
-          'availableCryptoCoin',
-          +refreshedCryptoCoinBalance,
-        );
-
-        await sendToRecipients(`SELL
-                 STRATEGY 1.2(RSI + DMI)
-                 Deal №: ${botState.dealsCount}
-                 Symbol: ${symbol.toUpperCase()}
-                 Price: ${botState.order.fills[0].price} USDT
-                 Date: ${format(new Date(), DATE_FORMAT)}
-                 Current profit: ${
-                   botState.currentProfit
-                 } USDT (${expectedProfitPercent} %)
-                 Total profit: ${botState.totalProfit} USDT 
-                 Average deal profit: ${botState.totalProfit /
-                   botState.dealsCount} USDT/deal
-                 Stablecoin balance: ${botState.availableUSDT} USDT
-                 Cryptocoin balance: ${+botState.availableCryptoCoin} ${cryptoCoin}
-                 OrderInfo: ${JSON.stringify(botState.order)}
-                 Work duration: ${format(
-                   botState.startTime - new Date().getTime(),
-                   DATE_FORMAT,
-                 )}
-             `);
-
-        // console.log(`Sell
-        //                     STRATEGY 1.2 (RSI + DMI)
-        //                     symbol: ${symbol.toUpperCase()}
-        //                     price: ${currentPrice}
-        //                     date: ${format(new Date(), DATE_FORMAT)}
-        //                     current profit: ${Number(profit - 0.2).toPrecision(
-        //                       4,
-        //                     )}%
-        //       `);
-        botState.dealsCount++;
-        botState.updateState('status', 'buy');
-      } catch (e) {
-        await sendToRecipients(`SELL ERROR
-            ${JSON.stringify(e)}
-      `);
-        botState.updateState('status', 'sell');
-      }
-    }
   };
 
   getRsiStream({
@@ -345,6 +248,15 @@ import { getEmaStream } from './indicators/ema';
     period: 14,
     interval: '1m',
   }).subscribe(rsi => {
+    if (!indicatorsData.rsi1mValue) {
+      indicatorsData.rsi1mValue = rsi;
+      return;
+    }
+    if (
+        (indicatorsData.rsi1mValue > 50 && rsi < 50) ||
+        (indicatorsData.rsi1mValue < 50 && rsi > 50)
+    )
+      indicatorsData.directionalMovementSignalWeight = 0;
     indicatorsData.rsi1mValue = rsi;
   });
 
@@ -370,7 +282,26 @@ import { getEmaStream } from './indicators/ema';
     if (dmi.mdi - dmi.pdi >= 2) indicatorsData.mdi1mSignal = -1;
     if (dmi.pdi - dmi.mdi >= 2) indicatorsData.mdi1mSignal = 1;
     if (indicatorsData.prev1mDmi.adx === dmi.adx) return;
-    indicatorsData.prev1mDmi = dmi.adx < indicatorsData.prev1mDmi.adx;
+    indicatorsData.isDirectionalMovementChanged = indicatorsData.prev1mDmi > dmi.adx;
+    if (indicatorsData.rsi1mValue < 50 && indicatorsData.isDirectionalMovementChanged)
+      console.log('BUY');
+    else if (
+        indicatorsData.rsi1mValue < 50 &&
+        !indicatorsData.isDirectionalMovementChanged
+    )
+      console.log('SELL');
+    else if (
+        indicatorsData.rsi1mValue > 50 &&
+        indicatorsData.isDirectionalMovementChanged
+    )
+      console.log('SELL');
+    else if (
+        indicatorsData.rsi1mValue > 50 &&
+        !indicatorsData.isDirectionalMovementChanged
+    )
+      console.log('BUY');
+    indicatorsData.prev1mDmi = dmi.adx;
+
   });
 
   getDmiStream({
@@ -378,61 +309,123 @@ import { getEmaStream } from './indicators/ema';
     interval: '1h',
     period: 14,
   }).subscribe(dmi => {
-    if (dmi.mdi > dmi.pdi) indicatorsData.mdi1hSignal = -1;
-    if (dmi.pdi > dmi.mdi) indicatorsData.mdi1hSignal = 1;
+    if (!indicatorsData.prev1hDmi) {
+      indicatorsData.prev1hDmi = dmi;
+      return;
+    }
+    // if (
+    //   indicatorsData.prev1mDmi.mdi > indicatorsData.prev1mDmi.pdi &&
+    //   dmi.pdi > dmi.mdi
+    // ) {
+    //   indicatorsData.trend = 'UP';
+    //   indicatorsData.directionalMovementSignalWeight = 0;
+    // }
+    // if (
+    //   indicatorsData.prev1mDmi.mdi < indicatorsData.prev1mDmi.pdi &&
+    //   dmi.pdi < dmi.mdi
+    // ) {
+    //   indicatorsData.trend = 'DOWN';
+    //   indicatorsData.directionalMovementSignalWeight = 0;
+    // }
+    // // console.log('Trend: ' + indicatorsData.trend);
+    // console.log(dmi);
+    // console.log(indicatorsData.prev1mDmi);
+    if (dmi.adx > dmi.pdi) indicatorsData.adx1hSignal = -1;
+    if (dmi.pdi > dmi.adx) indicatorsData.adx1hSignal = 1;
+    if (dmi.mdi > dmi.pdi) {
+      indicatorsData.mdi1hSignal = -1;
+      indicatorsData.trend = 'DOWN';
+    }
+    if (dmi.pdi > dmi.mdi) {
+      indicatorsData.mdi1hSignal = 1;
+      indicatorsData.trend = 'UP';
+    }
+    console.log(indicatorsData.trend);
+    // if (indicatorsData.prev1mDmi.adx === dmi.adx) return;
+
+    indicatorsData.isDirectionalMovementChanged =
+        indicatorsData.prev1hDmi.adx > dmi.adx;
+    if (
+        indicatorsData.trend === 'DOWN' &&
+        indicatorsData.isDirectionalMovementChanged
+    ) {
+      indicatorsData.directionalMovementSignalWeight++;
+      console.log(
+          'Weight for BUY: ' +
+          indicatorsData.directionalMovementSignalWeight.toString(),
+      );
+    } else if (
+        indicatorsData.trend === 'DOWN' &&
+        !indicatorsData.isDirectionalMovementChanged
+    ) {
+    } else if (
+        indicatorsData.trend === 'UP' &&
+        indicatorsData.isDirectionalMovementChanged
+    ) {
+      indicatorsData.directionalMovementSignalWeight++;
+      console.log(
+          'Weight for SELL: ' +
+          indicatorsData.directionalMovementSignalWeight.toString(),
+      );
+    } else if (
+        indicatorsData.trend === 'UP' &&
+        !indicatorsData.isDirectionalMovementChanged
+    ) {
+    }
+    indicatorsData.prev1hDmi = dmi;
   });
 
-  getEmaStream({
-    symbol: symbol,
-    interval: '1m',
-    period: 7,
-  }).subscribe(fastEMA => {
-    indicatorsData.fast1mEMA = fastEMA;
-  });
+  // getEmaStream({
+  //   symbol: symbol,
+  //   interval: '1m',
+  //   period: 7,
+  // }).subscribe(fastEMA => {
+  //   indicatorsData.fast1mEMA = fastEMA;
+  // });
 
-  getEmaStream({
-    symbol: symbol,
-    interval: '1m',
-    period: 25,
-  }).subscribe(middleEMA => {
-    indicatorsData.middle1mEMA = middleEMA;
-  });
+  // getEmaStream({
+  //   symbol: symbol,
+  //   interval: '1m',
+  //   period: 25,
+  // }).subscribe(middleEMA => {
+  //   indicatorsData.middle1mEMA = middleEMA;
+  // });
 
-  getEmaStream({
-    symbol: symbol,
-    interval: '1m',
-    period: 99,
-  }).subscribe(slowEMA => {
-    indicatorsData.slow1mEMA = slowEMA;
-  });
-
-  getEmaStream({
-    symbol: symbol,
-    interval: '1h',
-    period: 7,
-  }).subscribe(fastEMA => {
-    indicatorsData.fast1hEMA = fastEMA;
-  });
-
-  getEmaStream({
-    symbol: symbol,
-    interval: '1h',
-    period: 25,
-  }).subscribe(middleEMA => {
-    indicatorsData.middle1hEMA = middleEMA;
-  });
-
-  getEmaStream({
-    symbol: symbol,
-    interval: '1h',
-    period: 99,
-  }).subscribe(slowEMA => {
-    indicatorsData.slow1hEMA = slowEMA;
-  });
+  // getEmaStream({
+  //   symbol: symbol,
+  //   interval: '1m',
+  //   period: 99,
+  // }).subscribe(slowEMA => {
+  //   indicatorsData.slow1mEMA = slowEMA;
+  // });
+  //
+  // getEmaStream({
+  //   symbol: symbol,
+  //   interval: '1h',
+  //   period: 7,
+  // }).subscribe(fastEMA => {
+  //   indicatorsData.fast1hEMA = fastEMA;
+  // });
+  //
+  // getEmaStream({
+  //   symbol: symbol,
+  //   interval: '1h',
+  //   period: 25,
+  // }).subscribe(middleEMA => {
+  //   indicatorsData.middle1hEMA = middleEMA;
+  // });
+  //
+  // getEmaStream({
+  //   symbol: symbol,
+  //   interval: '1h',
+  //   period: 99,
+  // }).subscribe(slowEMA => {
+  //   indicatorsData.slow1hEMA = slowEMA;
+  // });
 
   await sendToRecipients(`INIT
   Bot started working at: ${format(new Date(), DATE_FORMAT)}
-  with using the STRATEGY 1.2(RSI + DMI) (LAST MODIFIED)
+  with using the ADX STRATEGY
   Symbol: ${symbol.toUpperCase()}
   Initial USDT balance: ${initialUSDTBalance} USDT
   Initial ${cryptoCoin} balance: ${initialCryptoCoinBalance} ${cryptoCoin}
--- src/index.ts
+++ src/index.ts
@@ -1,145 +1,296 @@
-import { combineLatest } from 'rxjs';
-import { map, pluck } from 'rxjs/operators';
+import { pluck, bufferCount } from 'rxjs/operators';
 import { format } from 'date-fns';
-
 import { connect } from './db/connection';
-import { SYMBOLS } from './constants';
+import { RESOURCES } from './constants';
 import { DATE_FORMAT } from './constants/date';
-import { getCandleStreamForInterval } from './api/candles';
-import { transformRsiToSignal } from './tools/rsi-tool';
-import { makeVerticalVolumeToolStream } from './tools/vertical-volume-tool';
-import { makeStrategy } from './strategies/make-strategy';
-import { BUY, SELL } from './tools/signals';
-
+import { getTradeStream } from './api/trades.js';
 import { processSubscriptions, sendToRecipients } from './services/telegram';
 import { getRsiStream } from './indicators/rsi';
 import { getDmiStream } from './indicators/dmi';
+import { binance } from './api/binance';
+import getBalances from './api/balance';
+import { getExchangeInfo } from './api/exchangeInfo';
+import { marketSell, marketBuy } from './api/order';
 import { getEmaStream } from './indicators/ema';
 
 (async function() {
-  // await connect();
+  await connect();
   // await processSubscriptions();
+  const symbol = 'erdusdt';
+  const cryptoCoin = symbol.toUpperCase().slice(0, -4);
+  const { available: initialUSDTBalance } = await getBalances('USDT');
+  const { available: initialCryptoCoinBalance } = await getBalances(cryptoCoin);
+  const { stepSize } = await getExchangeInfo(symbol.toUpperCase(), 'LOT_SIZE');
 
-  const interval = '1m';
-  const symbol = 'adausdt';
-  let slow1mEMA = 0;
-  let middle1mEMA = 0;
-  let fast1mEMA = 0;
-  let ema1mSignal = null;
+  // const symbol = process.argv[2];
+  const botState = {
+    status: 'buy',
+    currentProfit: null,
+    totalProfit: null,
+    tradeAmountPercent: 0.95,
+    availableUSDT: initialUSDTBalance,
+    availableCryptoCoin: initialCryptoCoinBalance,
+    buyPrice: null,
+    currentPrice: null,
+    order: null,
+    avrDealProfit: null,
+    dealsCount: 1,
+    startTime: new Date().getTime(),
+    workDuration: null,
+    updateState: function(fieldName, value) {
+      this[`${fieldName}`] = value;
+    },
+  };
 
-  // const candlePrices$ = getCandleStreamForInterval(
-  //   SYMBOLS.BTCUSDT,
-  //   interval,
-  // ).pipe(pluck('closePrice'), map(Number));
-  //
-  // const volumes$ = makeVerticalVolumeToolStream(
-  //   {
-  //     interval,
-  //     symbol,
-  //   },
-  //   {
-  //     minimalLatestCandleVolume: 30,
-  //     minimalPercentageIncrease: 20,
-  //   },
-  // );
+  const indicatorsData = {
+    prev1mDmi: null,
+    prev1hDmi: null,
+    dmiMdi1hSignal: 0,
+    rsi1mValue: null,
+    rsi1hValue: null,
+    adx1mSignal: 0,
+    mdi1mSignal: 0,
+    mdi1hSignal: 0,
+    slow1mEMA: 0,
+    middle1mEMA: 0,
+    fast1mEMA: 0,
+    slow1hEMA: 0,
+    middle1hEMA: 0,
+    fast1hEMA: 0,
+    isDownTrend: false,
+    rebuy: true,
+    isDirectionalMovementChanged: false,
+    directionalMovementSignalWeight: 0,
+    trend: null,
+  };
 
-  const rsiSignals$ = getRsiStream({
-    symbol: SYMBOLS.BTCUSDT,
+  const trader = async pricesStream => {
+    const { tradeAmountPercent } = botState;
+    const {
+      rsi1mValue,
+      rsi1hValue,
+      adx1mSignal,
+      mdi1mSignal,
+      mdi1hSignal,
+      slow1mEMA,
+      fast1mEMA,
+      middle1mEMA,
+      fast1hEMA,
+      middle1hEMA,
+      slow1hEMA,
+      isDownTrend,
+      rebuy,
+    } = indicatorsData;
+
+    if (botState.status === 'isPending') return;
+    botState.updateState(
+      'currentPrice',
+      Number(pricesStream[pricesStream.length - 1]),
+    );
+    let ema1mSignal;
+    let ema1hSignal;
+    if (fast1mEMA > middle1mEMA && middle1mEMA > slow1mEMA) {
+      ema1mSignal = 1;
+      // indicatorsData.isDownTrend = false;
+      indicatorsData.rebuy = true;
+    }
+    if (fast1mEMA < middle1mEMA && middle1mEMA < slow1mEMA) ema1mSignal = -1;
+
+    if (fast1hEMA > middle1hEMA) {
+      ema1hSignal = 1;
+      indicatorsData.isDownTrend = false;
+    }
+    if (fast1hEMA < middle1hEMA) ema1hSignal = -1;
+
+    const expectedProfitPercent = botState.buyPrice
+      ? botState.currentPrice / botState.buyPrice > 1
+        ? Number((botState.currentPrice / botState.buyPrice) * 100 - 100)
+        : Number(-1 * (100 - (botState.currentPrice / botState.buyPrice) * 100))
+      : 0;
+    // const expectedStableCoinProfit =
+    //   (botState.availableCryptoCoin * botState.currentPrice * 0.999) /
+    //     botState.availableUSDT >
+    //   1
+    //     ? Number(
+    //         ((botState.availableCryptoCoin * botState.currentPrice) /
+    //           botState.availableUSDT) *
+    //           100 -
+    //           100,
+    //       )
+    //     : Number(
+    //         100 -
+    //           ((botState.availableCryptoCoin * botState.currentPrice) /
+    //             botState.availableUSDT) *
+    //             100,
+    //       );
+  };
+
+  getRsiStream({
+    symbol: symbol,
     period: 14,
     interval: '1m',
-  }).pipe(
-    transformRsiToSignal({
-      overbought: [50, 70],
-      oversold: [30, 50],
-    }),
-  );
-
-  // getDmiStream({
-  //   symbol: SYMBOLS.BTCUSDT,
-  //   interval: '1m',
-  //   period: 14,
-  // }).subscribe(data => console.log('data', data));
-
-  getEmaStream({
-    symbol: 'adausdt',
-    interval: '1m',
-    period: 7,
-  }).subscribe(data => {
-    fast1mEMA = data;
-  });
-  getEmaStream({
-    symbol: 'adausdt',
-    interval: '1m',
-    period: 25,
-  }).subscribe(data => {
-    middle1mEMA = data;
-  });
-  getEmaStream({
-    symbol: 'adausdt',
-    interval: '1m',
-    period: 99,
-  }).subscribe(data => {
-    if (!slow1mEMA) {
-      slow1mEMA = data;
+  }).subscribe(rsi => {
+    if (!indicatorsData.rsi1mValue) {
+      indicatorsData.rsi1mValue = rsi;
       return;
     }
-    slow1mEMA = data;
-    if (fast1mEMA > middle1mEMA && middle1mEMA > slow1mEMA) ema1mSignal = 'buy';
-    if (fast1mEMA < slow1mEMA) ema1mSignal = 'sell';
-    console.log('fast/middle', ((fast1mEMA / middle1mEMA) * 100).toString());
-    console.log('middle/slow', ((middle1mEMA / slow1mEMA) * 100).toString());
-    // console.log(ema1mSignal);
+    indicatorsData.rsi1mValue = rsi;
   });
 
-  // const strategy$ = makeStrategy({
-  //   buyTools: [volumes$],
-  //   sellTools: [rsiSignals$],
+  getRsiStream({
+    symbol: symbol,
+    period: 14,
+    interval: '1h',
+  }).subscribe(rsi => {
+    indicatorsData.rsi1hValue = rsi;
+  });
+
+  getDmiStream({
+    symbol: symbol,
+    interval: '1h',
+    period: 14,
+  }).subscribe(dmi => {
+    if (!indicatorsData.prev1mDmi) {
+      indicatorsData.prev1mDmi = dmi;
+      return;
+    }
+    // if (
+    //   indicatorsData.prev1mDmi.mdi > indicatorsData.prev1mDmi.pdi &&
+    //   dmi.pdi > dmi.mdi
+    // ) {
+    //   indicatorsData.trend = 'UP';
+    //   indicatorsData.directionalMovementSignalWeight = 0;
+    // }
+    // if (
+    //   indicatorsData.prev1mDmi.mdi < indicatorsData.prev1mDmi.pdi &&
+    //   dmi.pdi < dmi.mdi
+    // ) {
+    //   indicatorsData.trend = 'DOWN';
+    //   indicatorsData.directionalMovementSignalWeight = 0;
+    // }
+    // // console.log('Trend: ' + indicatorsData.trend);
+    // console.log(dmi);
+    // console.log(indicatorsData.prev1mDmi);
+    if (dmi.adx > dmi.pdi) indicatorsData.adx1mSignal = -1;
+    if (dmi.pdi > dmi.adx) indicatorsData.adx1mSignal = 1;
+    if (dmi.mdi > dmi.pdi) {
+      indicatorsData.mdi1mSignal = -1;
+      indicatorsData.trend = 'DOWN';
+    }
+    if (dmi.pdi > dmi.mdi) {
+      indicatorsData.mdi1mSignal = 1;
+      indicatorsData.trend = 'UP';
+    }
+    console.log(indicatorsData.trend);
+    // if (indicatorsData.prev1mDmi.adx === dmi.adx) return;
+
+    indicatorsData.isDirectionalMovementChanged =
+      indicatorsData.prev1mDmi.adx > dmi.adx;
+    if (
+      indicatorsData.trend === 'DOWN' &&
+      indicatorsData.isDirectionalMovementChanged
+    ) {
+      indicatorsData.directionalMovementSignalWeight++;
+      console.log(
+        'Weight for BUY: ' +
+          indicatorsData.directionalMovementSignalWeight.toString(),
+      );
+    } else if (
+      indicatorsData.trend === 'DOWN' &&
+      !indicatorsData.isDirectionalMovementChanged
+    ) {
+    } else if (
+      indicatorsData.trend === 'UP' &&
+      indicatorsData.isDirectionalMovementChanged
+    ) {
+      indicatorsData.directionalMovementSignalWeight++;
+      console.log(
+        'Weight for SELL: ' +
+          indicatorsData.directionalMovementSignalWeight.toString(),
+      );
+    } else if (
+      indicatorsData.trend === 'UP' &&
+      !indicatorsData.isDirectionalMovementChanged
+    ) {
+    }
+    indicatorsData.prev1mDmi = dmi;
+  });
+
+  // getDmiStream({
+  //   symbol: symbol,
+  //   interval: '1h',
+  //   period: 14,
+  // }).subscribe(dmi => {
+  //   if (dmi.mdi > dmi.pdi) indicatorsData.mdi1hSignal = -1;
+  //   if (dmi.pdi > dmi.mdi) indicatorsData.mdi1hSignal = 1;
   // });
-  //
-  // let hasBought = false;
-  //
+
+  getEmaStream({
+    symbol: symbol,
+    interval: '1m',
+    period: 7,
+  }).subscribe(fastEMA => {
+    indicatorsData.fast1mEMA = fastEMA;
+  });
+
+  getEmaStream({
+    symbol: symbol,
+    interval: '1m',
+    period: 25,
+  }).subscribe(middleEMA => {
+    indicatorsData.middle1mEMA = middleEMA;
+  });
+
+  getEmaStream({
+    symbol: symbol,
+    interval: '1m',
+    period: 99,
+  }).subscribe(slowEMA => {
+    indicatorsData.slow1mEMA = slowEMA;
+  });
+
+  getEmaStream({
+    symbol: symbol,
+    interval: '1h',
+    period: 7,
+  }).subscribe(fastEMA => {
+    indicatorsData.fast1hEMA = fastEMA;
+  });
+
+  getEmaStream({
+    symbol: symbol,
+    interval: '1h',
+    period: 25,
+  }).subscribe(middleEMA => {
+    indicatorsData.middle1hEMA = middleEMA;
+  });
+
+  getEmaStream({
+    symbol: symbol,
+    interval: '1h',
+    period: 99,
+  }).subscribe(slowEMA => {
+    indicatorsData.slow1hEMA = slowEMA;
+  });
+
   // await sendToRecipients(`INIT
-  // symbol: ${symbol}
-  // interval: ${interval}
-  // period: 14,
-  //
-  // buy via VOLUME
-  //   minimalLatestCandleVolume: 30
-  //   minimalPercentageIncrease: 20
-  // ---
-  // sell via RSI
-  //   overbought: [50, 70]
-  //   oversold: [30, 50],
+  // Bot started working at: ${format(new Date(), DATE_FORMAT)}
+  // with using the STRATEGY 1.2(RSI + DMI) (LAST MODIFIED)
+  // Symbol: ${symbol.toUpperCase()}
+  // Initial USDT balance: ${initialUSDTBalance} USDT
+  // Initial ${cryptoCoin} balance: ${initialCryptoCoinBalance} ${cryptoCoin}
   // `);
-  //
-  // combineLatest(strategy$, candlePrices$).subscribe(
-  //   async ([strategySignalDetails, price]) => {
-  //     const date = format(new Date(), DATE_FORMAT);
-  //
-  //     if (!hasBought && strategySignalDetails.action === BUY) {
-  //       await sendToRecipients(`BUY
-  //         price: ${price}
-  //         date: ${date}
-  //         signals: ${JSON.stringify(strategySignalDetails.signals)}
-  //       `);
-  //
-  //       hasBought = true;
-  //     }
-  //
-  //     if (hasBought && strategySignalDetails.action === SELL) {
-  //       await sendToRecipients(`SELL
-  //         price: ${price}
-  //         date: ${date}
-  //         signals: ${JSON.stringify(strategySignalDetails.signals)}
-  //       `);
-  //
-  //       hasBought = false;
-  //     }
-  //   },
-  // );
+
+  getTradeStream({
+    symbol: symbol,
+    resource: RESOURCES.TRADE,
+  })
+    .pipe(pluck('price'), bufferCount(1, 1))
+    .subscribe(trader);
 })();
 
 process.on('unhandledRejection', async (reason: Error) => {
+  console.error(reason);
   await sendToRecipients(`ERROR
     ${reason.message}
     ${reason.stack}
