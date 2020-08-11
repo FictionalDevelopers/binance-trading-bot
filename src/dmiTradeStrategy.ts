f8fb1a87101eb4ec20fb1b1cddb4e1ad7e7ca155
cheslasvegas <cheslasvegas@gmail.com>
2020-08-11 11:36:52 +0300

    KAVA

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -16,7 +16,7 @@ import { getRSISignal } from './components/rsi-signals';
 (async function() {
   await connect();
   // await processSubscriptions();
-  const symbol = 'zilusdt';
+  const symbol = 'kavausdt';
   const cryptoCoin = symbol.toUpperCase().slice(0, -4);
   const { available: initialUSDTBalance } = await getBalances('USDT');
   const { available: initialCryptoCoinBalance } = await getBalances(cryptoCoin);
