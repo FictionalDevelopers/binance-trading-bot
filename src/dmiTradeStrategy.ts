8aad1cf194915bfe08a978807b10d988830b414d
cheslasvegas <cheslasvegas@gmail.com>
2020-08-17 23:16:15 +0300

    ERD

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -21,7 +21,7 @@ import { getRSISignal } from './components/rsi-signals';
 (async function() {
   await connect();
   // await processSubscriptions();
-  const symbol = 'linkusdt';
+  const symbol = 'erdusdt';
   const cryptoCoin = symbol.toUpperCase().slice(0, -4);
   const { available: initialUSDTBalance } = await getBalances('USDT');
   const { available: initialCryptoCoinBalance } = await getBalances(cryptoCoin);
