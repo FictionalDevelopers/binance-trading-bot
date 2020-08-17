1ab21dac5018b31b878b3ce10ecb87f1655f5466
cheslasvegas <cheslasvegas@gmail.com>
2020-08-17 15:21:04 +0300

    Use ema stoploss

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -35,7 +35,7 @@ import { getRSISignal } from './components/rsi-signals';
     strategy: 'TRENDS CATCHER STRATEGY',
     testMode: false,
     useProfitLevels: false,
-    useEMAStopLoss: false,
+    useEMAStopLoss: true,
     status: lastOrder ? (lastOrder.side === 'SELL' ? 'buy' : 'sell') : 'BUY',
     // status: 'buy',
     profitLevels: {
