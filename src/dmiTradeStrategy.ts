f0a67f6e4aaec9aafdbd827a16e28540f3ea1f52
cheslasvegas <cheslasvegas@gmail.com>
2020-09-08 14:19:15 +0300

    REAL

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -28,7 +28,7 @@ import { getRSISignal } from './components/rsi-signals';
 
   const botState = {
     strategy: 'TRENDS CATCHER STRATEGY',
-    testMode: true,
+    testMode: false,
     useProfitLevels: false,
     useEMAStopLoss: false,
     status: lastOrder ? (lastOrder.side === 'SELL' ? 'buy' : 'sell') : 'BUY',
