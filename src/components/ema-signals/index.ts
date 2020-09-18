cdc0eee173204ea3d293f666f0bc2448ded6755a
cheslasvegas <cheslasvegas@gmail.com>
2020-09-18 11:19:55 +0300

    Commented EMA timer

--- src/components/ema-signals/index.ts
+++ src/components/ema-signals/index.ts
@@ -1,32 +1,32 @@
 import { getEmaStream } from '../../indicators/ema';
 import { indicatorsData } from '../../index';
 
-const timer = setInterval(() => {
-  if (!indicatorsData.ema25Prev) {
-    indicatorsData.ema25Prev = Number(indicatorsData.slow1mEMA).toFixed(4);
-    return;
-  }
-
-  if (
-    (Number(indicatorsData.slow1mEMA).toFixed(4) / indicatorsData.ema25Prev) *
-      100 >=
-    0.4
-  ) {
-    indicatorsData.emaBuySignal = true;
-    indicatorsData.emaSellSignal = false;
-  } else if (
-    (indicatorsData.ema25Prev / Number(indicatorsData.slow1mEMA).toFixed(4)) *
-      100 >=
-    0.4
-  ) {
-    indicatorsData.emaSellSignal = true;
-    indicatorsData.emaBuySignal = false;
-  }
-  console.log('Prev: ' + indicatorsData.ema25Prev);
-  indicatorsData.ema25Prev = Number(indicatorsData.slow1mEMA).toFixed(4);
-
-  console.log('Current: ' + indicatorsData.slow1mEMA);
-}, 60000);
+// const timer = setInterval(() => {
+//   if (!indicatorsData.ema25Prev) {
+//     indicatorsData.ema25Prev = Number(indicatorsData.slow1mEMA).toFixed(4);
+//     return;
+//   }
+//
+//   if (
+//     (Number(indicatorsData.slow1mEMA).toFixed(4) / indicatorsData.ema25Prev) *
+//       100 >=
+//     0.4
+//   ) {
+//     indicatorsData.emaBuySignal = true;
+//     indicatorsData.emaSellSignal = false;
+//   } else if (
+//     (indicatorsData.ema25Prev / Number(indicatorsData.slow1mEMA).toFixed(4)) *
+//       100 >=
+//     0.4
+//   ) {
+//     indicatorsData.emaSellSignal = true;
+//     indicatorsData.emaBuySignal = false;
+//   }
+//   console.log('Prev: ' + indicatorsData.ema25Prev);
+//   indicatorsData.ema25Prev = Number(indicatorsData.slow1mEMA).toFixed(4);
+//
+//   console.log('Current: ' + indicatorsData.slow1mEMA);
+// }, 60000);
 
 export const getEMASignal = (symbol, timeFrame, indicatorsData) => {
   getEmaStream({
