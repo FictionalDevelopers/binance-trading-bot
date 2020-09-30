fac742dfc8baee5a0d2793e4e051afa339d47781
cheslasvegas <cheslasvegas@gmail.com>
2020-09-30 09:12:40 +0300

    KAVA

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -16,7 +16,7 @@ import { getRSISignal } from './components/rsi-signals';
 (async function() {
   await connect();
   // await processSubscriptions();
-  const symbol = 'linkusdt';
+  const symbol = 'kavausdt';
   const cryptoCoin = symbol.toUpperCase().slice(0, -4);
   const { available: initialUSDTBalance } = await getBalances('USDT');
   const { available: initialCryptoCoinBalance } = await getBalances(cryptoCoin);
@@ -177,7 +177,7 @@ import { getRSISignal } from './components/rsi-signals';
         (indicatorsData.fast15mEMA / indicatorsData.middle15mEMA) * 100 - 100,
       ) >= 0.1 &&
       indicatorsData.fast1mEMA < indicatorsData.slow1mEMA &&
-      // indicatorsData.fast1hEMA > indicatorsData.middle1hEMA &&
+      indicatorsData.fast1hEMA > indicatorsData.middle1hEMA &&
       indicatorsData.rsi1m.rsiValue < 35
     ) {
       if (botState.testMode) {
@@ -256,9 +256,7 @@ import { getRSISignal } from './components/rsi-signals';
     }
     if (
       botState.status === 'sell' &&
-      (Number(
-        (indicatorsData.middle15mEMA / indicatorsData.fast15mEMA) * 100 - 100,
-      ) >= 0.1 ||
+      (indicatorsData.middle15mEMA > indicatorsData.fast15mEMA ||
         (indicatorsData.rsi1m.rsiValue >= 67 &&
           indicatorsData.fast1mEMA > indicatorsData.slow1mEMA &&
           expectedProfitPercent > 0))
@@ -348,7 +346,7 @@ import { getRSISignal } from './components/rsi-signals';
   getRSISignal(symbol, '1m', indicatorsData.rsi1m);
   getEMASignal(symbol, '15m', indicatorsData);
   getEMASignal(symbol, '1m', indicatorsData);
-  // getEMASignal(symbol, '1h', indicatorsData);
+  getEMASignal(symbol, '1h', indicatorsData);
   // getEMASignal(symbol, '15m', indicatorsData);
   // getEMASignal(symbol, '1h', indicatorsData);
 
