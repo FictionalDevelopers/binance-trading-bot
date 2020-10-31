da2d7e00fd55e3bcd908d92068784a8ab26a1b49
cheslasvegas <cheslasvegas@gmail.com>
2020-10-31 10:31:35 +0200

    Fixes

--- src/components/stochRSI-signals/index.ts
+++ src/components/stochRSI-signals/index.ts
@@ -13,12 +13,18 @@ export const getStochRSISignal = (
     interval: timeFrame,
   }).subscribe(stochRsi => {
     if (Number(stochRsi.k) - Number(stochRsi.d) >= buySens) {
-      if (indicatorsData.stochRsiSignal[`stoch${timeFrame}`] === 'sell')
+      if (
+        indicatorsData.stochRsiSignal[`stoch${timeFrame}`] === 'sell' ||
+        indicatorsData.stochRsiSignal[`stoch${timeFrame}`] === null
+      )
         indicatorsData.stochRsiSignal[`stoch${timeFrame}`] = 'buy';
     }
 
     if (Number(stochRsi.d) - Number(stochRsi.k) >= sellSens) {
-      if (indicatorsData.stochRsiSignal[`stoch${timeFrame}`] === 'buy')
+      if (
+        indicatorsData.stochRsiSignal[`stoch${timeFrame}`] === 'buy' ||
+        indicatorsData.stochRsiSignal[`stoch${timeFrame}`] === null
+      )
         indicatorsData.stochRsiSignal[`stoch${timeFrame}`] = 'sell';
     }
     // console.log(`StochRSI:${JSON.stringify(stochRsi)}`);
