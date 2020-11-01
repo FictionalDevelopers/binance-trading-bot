6f166ed0cfa0e5259b6f3a7c2d93b86208d74e1c
cheslasvegas <cheslasvegas@gmail.com>
2020-11-02 00:07:00 +0200

    Implemented dataset update from local

---
+++ src/setBotStateData.ts
@@ -0,0 +1,46 @@
+import { connect } from './db/connection';
+import { sendToRecipients } from './services/telegram';
+
+import { service as botStateService } from './components/botState';
+
+const botState = {
+  avrDealProfit: null,
+  buyPrice: 0,
+  buyReason: 'upTrend',
+  cummulativeQuoteQty: null,
+  currentPrice: 0,
+  currentProfit: null,
+  dealsCount: 1,
+  emaStartPoint: null,
+  enabledLimits: false,
+  order: null,
+  prevPrice: 0,
+  sellError: false,
+  status: 'buy',
+  strategies: {
+    upTrend: { enabled: false, stopLoss: false },
+    downTrend: { enabled: false, stopLoss: false },
+    upFlat: { enabled: true, stopLoss: false },
+    downFlat: { enabled: false, stopLoss: false },
+    stochRsi: { enabled: false, stopLoss: false },
+    trendsCatcher: { enabled: false, stopLoss: false },
+  },
+  testMode: true,
+  totalPercentProfit: null,
+  totalProfit: 0,
+  tradeAmountPercent: 0.95,
+};
+
+(async function() {
+  await connect();
+  try {
+    await botStateService.trackBotState(botState);
+    await sendToRecipients(`DATASET WAS UPDATED`);
+  } catch (e) {
+    await sendToRecipients(`DATASET UPDATE ERROR
+    ${JSON.stringify(e)};
+  `);
+
+    process.exit(1);
+  }
+})();
