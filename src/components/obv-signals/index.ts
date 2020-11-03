a876faec30dccc714b8e5398f0bbcc77a59025e9
cheslasvegas <cheslasvegas@gmail.com>
2020-11-03 16:07:50 +0200

    Fixed obv

--- src/components/obv-signals/index.ts
+++ src/components/obv-signals/index.ts
@@ -5,13 +5,15 @@ export const getObvSignal = (symbol, timeFrame, indicatorsData) => {
     symbol: symbol,
     interval: timeFrame,
   }).subscribe(obv => {
-    if (!indicatorsData.obv) {
+    if (obv) {
+      if (!indicatorsData.obv) {
+        indicatorsData.obv = obv;
+        return;
+      }
+      if (indicatorsData.obv < obv) indicatorsData.obvSignal = 'buy';
+      if (indicatorsData.obv > obv) indicatorsData.obvSignal = 'sell';
       indicatorsData.obv = obv;
-      return;
+      console.log(obv);
     }
-    if (indicatorsData.obv < obv) indicatorsData.obvSignal = 'buy';
-    if (indicatorsData.obv > obv) indicatorsData.obvSignal = 'sell';
-    indicatorsData.obv = obv;
-    console.log(obv);
   });
 };
