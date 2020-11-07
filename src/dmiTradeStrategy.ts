4b13115ec36625faab825276a90df52949f6379c
cheslasvegas <cheslasvegas@gmail.com>
2020-11-07 10:13:43 +0200

    Changes

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -712,7 +712,7 @@ import getAvarage from './utils/getAverage';
   // getStochRSISignal(symbol, '1m', indicatorsData, 1.5, 1.5);
   // getStochRSISignal(symbol, '5m', indicatorsData, 1.5, 1.5);
   getStochRSISignal(symbol, '5m', indicatorsData, 5, 5);
-  getStochRSISignal(symbol, '1m', indicatorsData, 0.5, 1);
+  getStochRSISignal(symbol, '1m', indicatorsData, 5, 5);
   getDMISignal(symbol, '5m', indicatorsData.dmi5m);
   // getDMISignal(symbol, '1m', indicatorsData.dmi1m);
   // getStochRSISignal(symbol, '1h', indicatorsData);
