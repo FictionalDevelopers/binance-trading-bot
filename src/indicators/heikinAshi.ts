59714221b4a628bd5d81ffb7d05c4903fedace7b
Eugene Annienkov <eugene.annienkov@onereach.com>
2021-05-31 15:51:06 +0300

    chore: add heikinAshi stream

---
+++ src/indicators/heikinAshi.ts
@@ -0,0 +1,42 @@
+import { HeikinAshi } from 'technicalindicators';
+import { from, Observable } from 'rxjs';
+import { last } from 'lodash';
+import _map from 'lodash/map';
+import { map, pluck, switchMap } from 'rxjs/operators';
+import { getCandleStreamForInterval, getCandlesList } from '../api/candles';
+
+type HeikinAshiStreamConfig = {
+  symbol: string;
+  interval: string;
+};
+
+export const getHeikinAshiStream = (
+  config: HeikinAshiStreamConfig,
+): Observable<CandleList> =>
+  getCandleStreamForInterval(config.symbol, config.interval).pipe(
+    switchMap(_ => from(getCandlesList(config))),
+    map(
+      candles =>
+        new HeikinAshi({
+          low: _map(candles, candle => Number(candle.low)),
+          open: _map(candles, candle => Number(candle.open)),
+          high: _map(candles, candle => Number(candle.high)),
+          close: _map(candles, candle => Number(candle.close)),
+          volume: _map(candles, 'volume'),
+          timestamp: _map(candles, 'time'),
+        }),
+    ),
+    pluck('result'),
+    map(item => ({
+      open: last(item.open),
+      close: last(item.close),
+      low: last(item.low),
+      high: last(item.high),
+    })),
+  );
+
+(async function() {
+  await getHeikinAshiStream({ symbol: 'linkusdt', interval: '1m' }).subscribe(
+    data => console.log('data', data) || data,
+  );
+})();
