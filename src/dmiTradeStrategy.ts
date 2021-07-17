cc73aeed342f9c7170d824db2201c6459518d81b
cheslasvegas <cheslasvegas@gmail.com>
2021-07-17 15:21:45 +0300

    Fix 2

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -850,8 +850,8 @@ import {
               : botState.status === 'buy' &&
                 // indicatorsData.haCandle.ha1hCandle.signal === 'buy' &&
                 indicatorsData.obv1d.buySignalCount >= 20 &&
-                indicatorsData.obv4h.buySignalCount >= 20 &&
-                indicatorsData.obv1h.buySignalCount >= 20,
+                indicatorsData.obv4h.buySignalCount >= 50 &&
+                indicatorsData.obv1h.buySignalCount >= 50,
           // indicatorsData.obv5m.buySignalCount >= 4,
           // indicatorsData.obv1m.buySignalCount >= 20,
           // indicatorsData.haCandle.ha1hCandle.signal === 'buy',
@@ -861,8 +861,8 @@ import {
               : botState.status === 'buy' &&
                 // indicatorsData.haCandle.ha1hCandle.signal === 'sell' &&
                 indicatorsData.obv1d.sellSignalCount >= 20 &&
-                indicatorsData.obv4h.sellSignalCount >= 20 &&
-                indicatorsData.obv1h.sellSignalCount >= 20,
+                indicatorsData.obv4h.sellSignalCount >= 50 &&
+                indicatorsData.obv1h.sellSignalCount >= 50,
           // indicatorsData.obv5m.sellSignalCount >= 4,
           // indicatorsData.obv1m.sellSignalCount >= 20,
           // indicatorsData.haCandle.ha1hCandle.signal === 'sell',
