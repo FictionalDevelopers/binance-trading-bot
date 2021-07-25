51477d08428fcd191ac7cd4876d01932aa49219c
cheslasvegas <cheslasvegas@gmail.com>
2021-07-25 08:03:00 +0300

    5m

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -848,11 +848,11 @@ import {
             botState.initialDealType === 'short'
               ? null
               : botState.status === 'buy' &&
-                indicatorsData.haCandle.ha1hCandle.signal === 'buy' &&
+                // indicatorsData.haCandle.ha1hCandle.signal === 'buy' &&
                 // indicatorsData.obv1d.buySignalCount >= 20 &&
-                indicatorsData.obv4h.buySignalCount >= 20 &&
-                indicatorsData.obv1h.buySignalCount >= 20 &&
-                indicatorsData.obv15m.buySignalCount >= 20,
+                // indicatorsData.obv4h.buySignalCount >= 20 &&
+                // indicatorsData.obv1h.buySignalCount >= 20 &&
+                indicatorsData.obv5m.buySignalCount >= 30,
           // indicatorsData.obv5m.buySignalCount >= 4,
           // indicatorsData.obv1m.buySignalCount >= 20,
           // indicatorsData.haCandle.ha1hCandle.signal === 'buy',
@@ -860,11 +860,11 @@ import {
             botState.initialDealType === 'long'
               ? null
               : botState.status === 'buy' &&
-                indicatorsData.haCandle.ha1hCandle.signal === 'sell' &&
+                // indicatorsData.haCandle.ha1hCandle.signal === 'sell' &&
                 // indicatorsData.obv1d.sellSignalCount >= 20 &&
-                indicatorsData.obv4h.sellSignalCount >= 20 &&
-                indicatorsData.obv1h.sellSignalCount >= 20 &&
-                indicatorsData.obv15m.sellSignalCount >= 20,
+                // indicatorsData.obv4h.sellSignalCount >= 20 &&
+                // indicatorsData.obv1h.sellSignalCount >= 20 &&
+                indicatorsData.obv5m.sellSignalCount >= 30,
           // indicatorsData.obv5m.sellSignalCount >= 4,
           // indicatorsData.obv1m.sellSignalCount >= 20,
           // indicatorsData.haCandle.ha1hCandle.signal === 'sell',
@@ -877,9 +877,9 @@ import {
               botState.dealType === 'long' &&
               // indicatorsData.obv1d.sellSignalCount >= 20 ||
               // indicatorsData.obv15m.sellSignalCount >= 20 &&
-              indicatorsData.obv4h.sellSignalCount >= 20 &&
-              indicatorsData.obv1h.sellSignalCount >= 20 &&
-              indicatorsData.obv15m.sellSignalCount >= 20,
+              // indicatorsData.obv4h.sellSignalCount >= 20 &&
+              // indicatorsData.obv1h.sellSignalCount >= 20 &&
+              indicatorsData.obv5m.sellSignalCount >= 30,
             // indicatorsData.obv1m.sellSignalCount >= 20,
             // (indicatorsData.obv15m.sellSignalCount >= 2 &&
             //   indicatorsData.obv5m.sellSignalCount >= 2) ||
@@ -899,9 +899,9 @@ import {
               // indicatorsData.obv4h.buySignalCount >= 20 &&
               // indicatorsData.obv15m.buySignalCount >= 20 &&
               // indicatorsData.obv1d.buySignalCount >= 20 ||
-              indicatorsData.obv4h.buySignalCount >= 20 &&
-              indicatorsData.obv1h.buySignalCount >= 20 &&
-              indicatorsData.obv15m.buySignalCount >= 20,
+              // indicatorsData.obv4h.buySignalCount >= 20 &&
+              // indicatorsData.obv1h.buySignalCount >= 20 &&
+              indicatorsData.obv5m.buySignalCount >= 30,
             // indicatorsData.obv1m.buySignalCount >= 20,
             // (indicatorsData.obv15m.buySignalCount >= 2 &&
             //   indicatorsData.obv5m.buySignalCount >= 2) ||
@@ -1226,11 +1226,11 @@ import {
   // getObvSignal(symbol, '4h', indicatorsData.obv4h, 2, 2);
   // getObvSignal(symbol, '1h', indicatorsData.obv1h, 2, 2);
 
-  getHeikinAshiSignal(symbol, '1h', 6, 6, indicatorsData.haCandle.ha1hCandle);
+  // getHeikinAshiSignal(symbol, '1h', 6, 6, indicatorsData.haCandle.ha1hCandle);
   // getObvSignal(symbol, '1d', indicatorsData.obv1d, 20, 20);
-  getObvSignal(symbol, '4h', indicatorsData.obv4h, 20, 20);
-  getObvSignal(symbol, '1h', indicatorsData.obv1h, 20, 20);
-  getObvSignal(symbol, '15m', indicatorsData.obv15m, 20, 20);
+  // getObvSignal(symbol, '4h', indicatorsData.obv4h, 20, 20);
+  // getObvSignal(symbol, '1h', indicatorsData.obv1h, 20, 20);
+  getObvSignal(symbol, '5m', indicatorsData.obv5m, 20, 20);
   // getObvSignal(symbol, '5m', indicatorsData.obv5m, 10, 10);
   // getObvSignal(symbol, '1m', indicatorsData.obv1m, 10, 10);
   // getObvSignal(symbol, '1m', indicatorsData.obv1m, 10, 10);
