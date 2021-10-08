de2615b22cee910e93242623744baf0724dc5612
cheslasvegas <cheslasvegas@gmail.com>
2021-10-08 20:32:19 +0300

    OBV 5, 30 and EMA 99 1m

--- src/localBot.ts
+++ src/localBot.ts
@@ -1023,10 +1023,10 @@ import getAvarage from './utils/getAverage';
             botState.initialDealType === 'short'
               ? null
               : botState.status === 'buy' &&
-                // indicatorsData.obv30m.buySignalCount >= 20 &&
+                indicatorsData.obv30m.buySignalCount >= 20 &&
                 // indicatorsData.obv15m.buySignalCount >= 20 &&
                 indicatorsData.obv5m.buySignalCount >= 20 &&
-                indicatorsData.obv1m.buySignalCount >= 6 &&
+                // indicatorsData.obv1m.buySignalCount >= 6 &&
                 indicatorsData.ema.ema1m.slow.emaSignal === 'buy',
           // indicatorsData.ema.ema1m.fast.emaSignal === 'buy',
           // indicatorsData.obv1h.buySignalCount >= 30,
@@ -1048,10 +1048,10 @@ import getAvarage from './utils/getAverage';
             botState.initialDealType === 'long'
               ? null
               : botState.status === 'buy' &&
-                // indicatorsData.obv30m.sellSignalCount >= 20 &&
+                indicatorsData.obv30m.sellSignalCount >= 20 &&
                 // indicatorsData.obv15m.sellSignalCount >= 20 &&
                 indicatorsData.obv5m.sellSignalCount >= 20 &&
-                indicatorsData.obv1m.sellSignalCount >= 6 &&
+                // indicatorsData.obv1m.sellSignalCount >= 6 &&
                 indicatorsData.ema.ema1m.slow.emaSignal === 'sell',
           // indicatorsData.ema.ema1m.fast.emaSignal === 'sell',
 
@@ -1097,7 +1097,7 @@ import getAvarage from './utils/getAverage';
             long:
               botState.status === 'sell' &&
               botState.dealType === 'long' &&
-              indicatorsData.obv5m.sellSignalCount >= 6 &&
+              indicatorsData.obv5m.sellSignalCount >= 10 &&
               // indicatorsData.obv30m.sellSignalCount >= 20 &&
               // indicatorsData.obv15m.sellSignalCount >= 20 &&
               indicatorsData.ema.ema1m.slow.emaSignal === 'sell',
@@ -1135,7 +1135,7 @@ import getAvarage from './utils/getAverage';
             short:
               botState.status === 'sell' &&
               botState.dealType === 'short' &&
-              indicatorsData.obv5m.buySignalCount >= 6 &&
+              indicatorsData.obv5m.buySignalCount >= 10 &&
               // indicatorsData.obv15m.buySignalCount >= 20 &&
               // indicatorsData.obv30m.buySignalCount >= 20 &&
               indicatorsData.ema.ema1m.slow.emaSignal === 'buy',
@@ -1501,10 +1501,10 @@ import getAvarage from './utils/getAverage';
   // getObvSignal(symbol, '4h', indicatorsData.obv4h, 20, 20);
   // getEMASignal(symbol, '1m', indicatorsData.fast1mEMA)
   // getObvSignal(symbol, '1h', indicatorsData.obv1h, 60, 60);
-  // getObvSignal(symbol, '30m', indicatorsData.obv30m, 60, 60);
+  getObvSignal(symbol, '30m', indicatorsData.obv30m, 60, 60);
   // getObvSignal(symbol, '15m', indicatorsData.obv15m, 10, 10);
   getObvSignal(symbol, '5m', indicatorsData.obv5m, 6, 6);
-  getObvSignal(symbol, '1m', indicatorsData.obv1m, 30, 30);
+  // getObvSignal(symbol, '1m', indicatorsData.obv1m, 30, 30);
 
   const calculateEMADiff = (symbol, interval, period, indicatorsData) => {
     getEmaStream({
