08e257c00b5697fb1f51df6d80b703cbacfb1d73
cheslasvegas <cheslasvegas@gmail.com>
2021-12-23 11:47:29 +0200

    Disabled 5m CCI in the sell condition

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -1209,7 +1209,7 @@ import { getCRSIStream } from './indicators/crsi';
               (indicatorsData.obv15m.sellSignalCount >= 20 ||
                 indicatorsData.obv5m.sellSignalCount >= 20) &&
               // indicatorsData.cci.cci15m.cci < 0 &&
-              indicatorsData.cci.cci5m.cci < -100 &&
+              // indicatorsData.cci.cci5m.cci < -100 &&
               indicatorsData.cci.cci1m.cci < -100,
             // indicatorsData.crsi.crsi15m.crsi < 30 &&
             // indicatorsData.crsi.crsi5m.crsi < 30,
@@ -1283,7 +1283,7 @@ import { getCRSIStream } from './indicators/crsi';
               (indicatorsData.obv15m.buySignalCount >= 20 ||
                 indicatorsData.obv5m.buySignalCount >= 20) &&
               // indicatorsData.cci.cci15m.cci > 0 &&
-              indicatorsData.cci.cci5m.cci > 100 &&
+              // indicatorsData.cci.cci5m.cci > 100 &&
               indicatorsData.cci.cci1m.cci > 100,
             // indicatorsData.crsi.crsi15m.crsi > 70 &&
             // indicatorsData.crsi.crsi5m.crsi > 70,
