21e4198493f1a74e54a8b82e5edfb1164c908518
cheslasvegas <cheslasvegas@gmail.com>
2022-02-14 10:03:35 +0200

    RSI 1h 15m OBV 1h 15m

---
+++ src/1.js
@@ -0,0 +1,38 @@
+class Person {
+  constructor(age, name) {
+    this.name = name;
+    this.age = age;
+  }
+
+  method1() {
+    console.log('method1');
+    console.log(this);
+  }
+
+  static statMeth() {
+    console.log('Static');
+    console.log(this);
+  }
+
+  toString() {
+    console.log('Custom');
+  }
+}
+
+class Student extends Person {
+  constructor(name, age, salary) {
+    super(age, name);
+    this.salary = salary;
+  }
+
+  method1() {
+    super.method1();
+    // super.method1();
+  }
+
+  meth3() {
+    console.log(this);
+  }
+}
+const student = new Student('igor', 23, 5000);
+student.method1();
