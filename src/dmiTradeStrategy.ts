5084b0828ae46182051481f14873955a1f0c8dd7
cheslasvegas <cheslasvegas@gmail.com>
2022-12-18 10:47:16 +0100

    Sell only by ADX signal

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -1308,8 +1308,12 @@ import { getStochRSISignal } from './components/stochRSI-signals';
               ? null
               : botState.status === 'buy' &&
                 indicatorsData.obv1h.buySignalCount >= 10 &&
-                // indicatorsData.obv5m.buySignalCount >= 20 &&
-                indicatorsData.dmi1h.willPriceGrow,
+                indicatorsData.obv15m.buySignalCount >= 10 &&
+                indicatorsData.obv5m.buySignalCount >= 10 &&
+                indicatorsData.dmi1h.adxBuySignalVolume >= 1 &&
+                indicatorsData.dmi15m.adxBuySignalVolume >= 1 &&
+                indicatorsData.dmi5m.adxBuySignalVolume >= 1,
+          // indicatorsData.dmi1h.willPriceGrow,
           // (indicatorsData.dmi1h.adxBuySignalVolume >= 2 ||
           //   indicatorsData.dmi1h.adxSellSignalVolume >= 2),
 
@@ -1423,8 +1427,14 @@ import { getStochRSISignal } from './components/stochRSI-signals';
               ? null
               : botState.status === 'buy' &&
                 indicatorsData.obv1h.sellSignalCount >= 10 &&
-                // indicatorsData.obv5m.sellSignalCount >= 20 &&
-                indicatorsData.dmi1h.willPriceGrow,
+                indicatorsData.obv15m.sellSignalCount >= 10 &&
+                indicatorsData.obv5m.sellSignalCount >= 10 &&
+                indicatorsData.dmi1h.adxBuySignalVolume >= 1 &&
+                indicatorsData.dmi15m.adxBuySignalVolume >= 1 &&
+                indicatorsData.dmi5m.adxBuySignalVolume >= 1,
+          // indicatorsData.obv1h.sellSignalCount >= 10 &&
+          // indicatorsData.obv5m.sellSignalCount >= 20 &&
+          // indicatorsData.dmi1h.willPriceGrow,
 
           // (indicatorsData.dmi1h.adxBuySignalVolume >= 2 ||
           //   indicatorsData.dmi1h.adxSellSignalVolume >= 2),
@@ -1584,9 +1594,10 @@ import { getStochRSISignal } from './components/stochRSI-signals';
             long:
               botState.status === 'sell' &&
               botState.dealType === 'long' &&
-              indicatorsData.obv1h.sellSignalCount >= 10 &&
-              (indicatorsData.dmi1h.willPriceGrow ||
-                indicatorsData.dmi1h.adxSellSignalVolume >= 1),
+              indicatorsData.dmi1h.adxSellSignalVolume >= 1,
+            // indicatorsData.obv1h.sellSignalCount >= 10 &&
+            // (indicatorsData.dmi1h.willPriceGrow ||
+            //   indicatorsData.dmi1h.adxSellSignalVolume >= 1),
 
             // (indicatorsData.dmi1h.adxBuySignalVolume >= 2 ||
             //   indicatorsData.dmi1h.adxSellSignalVolume >= 2),
@@ -1724,9 +1735,11 @@ import { getStochRSISignal } from './components/stochRSI-signals';
             short:
               botState.status === 'sell' &&
               botState.dealType === 'short' &&
-              indicatorsData.obv1h.buySignalCount >= 10 &&
-              (indicatorsData.dmi1h.willPriceGrow ||
-                indicatorsData.dmi1h.adxSellSignalVolume >= 1),
+              indicatorsData.dmi1h.adxSellSignalVolume >= 1,
+
+            // indicatorsData.obv1h.buySignalCount >= 10 &&
+            // (indicatorsData.dmi1h.willPriceGrow ||
+            //   indicatorsData.dmi1h.adxSellSignalVolume >= 1),
 
             // (indicatorsData.dmi1h.adxBuySignalVolume >= 2 ||
             //   indicatorsData.dmi1h.adxSellSignalVolume >= 2),
