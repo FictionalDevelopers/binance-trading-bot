d968434dfbfbd2132d989507f811b436a09d78bf
cheslasvegas <cheslasvegas@gmail.com>
2023-04-05 23:28:21 +0200

    Added 15m RSI and OBV 1h to the log

--- src/api/order.ts
+++ src/api/order.ts
@@ -344,6 +344,7 @@ export const marketSellAction = async (
           } (UP: ${
             indicatorsData.avgPrices.avgSmall.avgPriceUpSignalCount
           } DOWN: ${indicatorsData.avgPrices.avgSmall.avgPriceDownSignalCount})
+          RSI 15m: ${indicatorsData.rsi15m.rsiValue}        
           RSI 5m: ${indicatorsData.rsi5m.rsiValue}        
           RSI 1m: ${indicatorsData.rsi1m.rsiValue}
           ADX 1m: ${'(UP: ' +
@@ -874,6 +875,7 @@ export const marketBuyAction = async (
         } (UP: ${
           indicatorsData.avgPrices.avgSmall.avgPriceUpSignalCount
         } DOWN: ${indicatorsData.avgPrices.avgSmall.avgPriceDownSignalCount})
+        RSI 15m: ${indicatorsData.rsi15m.rsiValue}        
         RSI 5m: ${indicatorsData.rsi5m.rsiValue}        
         RSI 1m: ${indicatorsData.rsi1m.rsiValue}        
        ADX 1m: ${'(UP: ' +
