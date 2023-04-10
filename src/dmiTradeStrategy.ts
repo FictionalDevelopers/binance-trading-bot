1e1ae63917e4a0a1f26ef4d6e0ec002d5eff2dd0
cheslasvegas <cheslasvegas@gmail.com>
2023-04-10 07:00:27 +0200

    ADX period from 4 to 2

--- src/dmiTradeStrategy.ts
+++ src/dmiTradeStrategy.ts
@@ -2345,9 +2345,9 @@ import { getStochRSISignal } from './components/stochRSI-signals';
   getObvSignal(symbol, '15m', indicatorsData.obv15m, 2, 2);
   getObvSignal(symbol, '5m', indicatorsData.obv5m, 60, 60);
   // getObvSignal(symbol, '1m', indicatorsData.obv1m, 60, 60);
-  getDMISignal(symbol, '1h', 4, indicatorsData.dmi1h, botState, true, false);
-  getDMISignal(symbol, '15m', 4, indicatorsData.dmi15m, botState, true, false);
-  getDMISignal(symbol, '5m', 4, indicatorsData.dmi5m, botState, true, false);
+  getDMISignal(symbol, '1h', 2, indicatorsData.dmi1h, botState, true, false);
+  getDMISignal(symbol, '15m', 2, indicatorsData.dmi15m, botState, true, false);
+  getDMISignal(symbol, '5m', 2, indicatorsData.dmi5m, botState, true, false);
   // getDMISignal(symbol, '1m', 2, indicatorsData.dmi1m, botState, true, false);
 
   // getObvSignal(symbol, '1d', indicatorsData.obv1d, 20, 20);
