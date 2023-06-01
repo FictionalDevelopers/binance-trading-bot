b0556cb4ce7fd1729963cc024fefea8d769b16d6
cheslasvegas <cheslasvegas@gmail.com>
2023-06-01 10:11:09 +0200

    For restart

--- src/1.js
+++ src/1.js
@@ -1,3 +1,3 @@
 console.log(+Number(5.4567897).toFixed(3));
 console.log(Number(5.4567897).toPrecision(5));
-console.log(Number(5.4567897).toFixed(3));
+// console.log(Number(5.4567897).toFixed(3));
